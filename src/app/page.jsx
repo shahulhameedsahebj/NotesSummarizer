67c92a4dbaaa833abe0b475ab823403ad47566a5
shahulhameedsahebj <shahulhameedsahebj@hexaware.com>
2024-06-02 22:37:22 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,6 @@ const Home = () => {
     <div className={styles.textContainer}>
       <h1 className={styles.title}>Hackathon</h1>
       <hr />
-
       <p style={{ textAlign: "justify" }}>The Collection Notes Summarizer is a tool designed to summarize past collection follow-up details. Leveraging the power of LLM and NLP techniques, it analyzes collection notes and documents from previous interactions between collectors and customers.</p>
       <p style={{ textAlign: "justify" }}>By processing a large amount of information within seconds, it captures key details such as a previous promise to pay, the potential for a dispute, the reason for delays in payment etc. Additionally, it provides a succinct summary of each note  to assist the collectors with their follow-up activities.</p>
     </div>
